32f9ca52abb490977ef70d0217c66089fefcd189
Daniel-derf <danielpardinho321@gmail.com>
2024-11-29 10:47:50 -0300

    async reducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,23 +2,22 @@ import {
   decrement,
   increment,
   incrementByAmount,
+  incrementAsync,
 } from "../state/counter/counterSlice";
-import { RootState } from "../state/store";
+import { AppDispatch, RootState } from "../state/store";
 import { useDispatch, useSelector } from "react-redux";
 
 type Props = {};
 
 const Counter = (props: Props) => {
   const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   return (
     <div>
       <h2>{count}</h2>
       <div>
-        <button onClick={() => dispatch(incrementByAmount(10))}>
-          Increment
-        </button>
+        <button onClick={() => dispatch(incrementAsync(10))}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
     </div>
